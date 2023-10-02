421ab6ec6249a7cba4989788087dfad7830387c8
Alex Roh <roheunyoung@outlook.com>
2023-10-02 12:38:51 +0900

    Initial Commit
    - 활동, 시간 레코드 기능 추가
    - 파이 그래프로 하루 활동 확인 가능
    - 세이브/로드 기능 구현

--- stopwatch.py
+++ stopwatch.py
@@ -3,6 +3,7 @@ import tkinter as tk
 import matplotlib.pyplot as plt
 import pytz
 from tkinter import ttk, filedialog, messagebox
+from tkinter.font import BOLD, Font
 from datetime import datetime, timedelta
 from collections import defaultdict
 from matplotlib import cm
@@ -22,35 +23,56 @@ class TimeRecorder:
         # GUI Components
         default_activities = ['Study', 'Essentials', 'Procrastinating', 'Programming', 'Reading', 'Web-Surfing']
         self.activity_var = tk.StringVar()
+
+        # (Combobox) Current Activity
         self.activity_dropdown = ttk.Combobox(root, textvariable=self.activity_var, values=default_activities)
-        self.activity_dropdown.pack(pady=20)
 
-        self.timer_label = ttk.Label(root, text="Time: 00:00:00.000")
-        self.timer_label.pack()
+        # (Label) Timer
+        self.timer_label = ttk.Label(root, text="00:00:00.000", font=("Arial", 25, BOLD))
+        
+        # (Label) Date
+        self.date_label = ttk.Label(root, text=f"{datetime.now().strftime('%Y-%m-%d')}", font=("Arial", 13, BOLD))
 
-        # self.start_img = tk.PhotoImage(file="start.png")
-        # self.stop_img = tk.PhotoImage(file="stop.png")
+        # (Label) Creator
+        self.creator = ttk.Label(root, text=f"developed by Alex Roh")
 
+        # (Button) Start
         self.start_button = ttk.Button(root, text="Start", command=self.start_timer)
-        self.start_button.pack()
 
+        # (Button) Stop
         self.stop_button = ttk.Button(root, text="Stop", command=self.stop_timer)
-        self.stop_button.pack(pady=20)
 
-        self.date_label = ttk.Label(root, text=f"Today's Date: {datetime.now().strftime('%Y-%m-%d')}")
-        self.date_label.pack()
-
-        self.listbox = tk.Listbox(root, height=10, width=60)
-        self.listbox.pack(pady=20)
-
+        # (Button) Save
         self.save_button = ttk.Button(root, text="Save", command=self.save_sessions)
-        self.save_button.pack()
 
+        # (Button) Load
         self.load_button = ttk.Button(root, text="Load", command=self.load_sessions)
-        self.load_button.pack(pady=20)
 
+        # (Button) Show Graph
         self.show_graph_button = ttk.Button(root, text="Show Graph", command=self.show_graph)
-        self.show_graph_button.pack(pady=20)
+
+        # (Button) Clear
+        self.clear_button = ttk.Button(root, text="Clear", command=self.clear_sessions)
+
+        # (Listbox) Activity List
+        self.listbox = tk.Listbox(root, height=10, width=60)
+
+        # Place each component using grid
+        self.activity_dropdown.grid(row=0, column=0, pady=20, columnspan=2)
+        self.timer_label.grid(row=1, column=0, columnspan=2)
+        self.start_button.grid(row=2, column=0, pady=10, columnspan=2)
+        self.stop_button.grid(row=3, column=0, columnspan=2)
+        self.date_label.grid(row=4, column=0, padx=10, columnspan=2, sticky="se")
+        self.listbox.grid(row=5, column=0, pady=10, columnspan=2)
+        self.save_button.grid(row=6, column=0, columnspan=2)
+        self.load_button.grid(row=7, column=0, pady=10, columnspan=2)
+        self.show_graph_button.grid(row=8, column=0, pady=20, columnspan=2)
+
+        # Place the 'clear' button in the middle, spanning both columns
+        self.clear_button.grid(row=9, column=0, pady=10, columnspan=2, sticky="n")
+
+        # Place the 'creator' label to the right of the 'clear' button
+        self.creator.grid(row=9, column=1, pady=10, sticky="se")
 
         # Update timer method
         self.update_timer()
@@ -80,10 +102,15 @@ class TimeRecorder:
     def update_timer(self):
         if self.start_time:
             elapsed = self.get_current_datetime() - self.start_time
-            time_str = str(timedelta(seconds=int(elapsed.total_seconds())))
+            seconds = elapsed.total_seconds()
+            hours, remainder = divmod(seconds, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            milliseconds = int((seconds - int(seconds)) * 1000)
+            
+            time_str = f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}.{milliseconds:03}"
             self.timer_label.config(text=f"Time: {time_str}")
         else:
-            self.timer_label.config(text="Time: 00:00:00:00.000")
+            self.timer_label.config(text="Time: 00:00:00.000")
         self.root.after(50, self.update_timer)
 
     def update_activity_list(self):
@@ -113,6 +140,11 @@ class TimeRecorder:
                 self.update_activity_list()
             except Exception as e:
                 messagebox.showwarning("Error", "No sessions found to load!")
+
+    def clear_sessions(self):
+        self.sessions = []
+        self.start_time = None
+        self.update_activity_list()
 
     def make_autopct(self, values):
         def my_autopct(pct):
